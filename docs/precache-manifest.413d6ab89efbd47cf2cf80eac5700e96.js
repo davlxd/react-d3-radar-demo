dc9fbea88a01ea549865c800c94d21cafed7e74c
lxd <i@lxd.me>
2019-08-29 18:54:34 +1200

    Publish on docs/

---
+++ docs/precache-manifest.413d6ab89efbd47cf2cf80eac5700e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "227fb34f41019cbd6a08cfbcce2bfe60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c541069c5a42ee0fecd0",
+    "url": "/static/css/main.d3fe0eee.chunk.css"
+  },
+  {
+    "revision": "1b3f86c25ab1ac26ae72",
+    "url": "/static/js/2.dcf3b8bb.chunk.js"
+  },
+  {
+    "revision": "c541069c5a42ee0fecd0",
+    "url": "/static/js/main.1daba391.chunk.js"
+  },
+  {
+    "revision": "8f9c99a392b782b974bc",
+    "url": "/static/js/runtime~main.a432f4eb.js"
+  }
+]);
